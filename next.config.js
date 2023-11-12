7ce69843ca7922c020f519e3cdeaccb8e6a7c0a7
ak-yoshimatsu <codetrain.akihiro.yoshimatsu@gmail.com>
2023-11-12 14:23:56 +0900

    Image要素のエラー解消設定

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  /* <Image>要素はセキュリティ上の理由から、リモートホストから画像を取り込むには
+   * あらかじめnext.config.jsで宣言しておく必要がある。
+   * 他にも、protocol, port, pathnameなどの制限が可能。
+   */
+  images: {
+    remotePatterns: [
+      {
+        hostname: 'via.placeholder.com',
+      },
+    ]
+  }
+}
 
 module.exports = nextConfig
