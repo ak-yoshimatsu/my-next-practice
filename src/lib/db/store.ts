d7224bdf97187ecff373dc526a4df25166030f20
ak-yoshimatsu <codetrain.akihiro.yoshimatsu@gmail.com>
2023-11-14 21:15:43 +0900

    サーバーにzodを適用

--- src/lib/db/store.ts
+++ src/lib/db/store.ts
@@ -1,13 +1,33 @@
 'use server';
 
-import prisma from "./prisma"
+import prisma from "./prisma";
 import { redirect } from "next/navigation";
+import z from "zod";
 
-export async function storePhoto(formData: any) {
-  const input = {
-    photoId: parseInt(formData.get('id')),
+const schema = z.object({
+  id: z.string(),
+  title: z.string(),
+  thumbnailUrl: z.string(),
+  detail: z.string(),
+});
+
+export async function storePhoto(formData: FormData) {
+  const parsed = schema.safeParse({
+    id: formData.get('id'),
     title: formData.get('title'),
-    thumbnail: formData.get('thumbnailUrl'),
+    detail: formData.get('detail'),
+    thumbnailUrl: formData.get('thumbnailUrl'),
+  });
+
+  if (!parsed.success) {
+    console.log('バリデーションエラー', parsed.error);
+    return false
+  }
+
+  const input = {
+    photoId: parseInt(formData.get('id') as string),
+    title: formData.get('title') as string,
+    thumbnail: formData.get('thumbnailUrl') as string,
   }
 
   await prisma.photos.create({
